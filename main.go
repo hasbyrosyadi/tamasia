c7aabd59ff24a77921d56bfce2c1a080bbbacbd3
Hasby Rosyadi <hasbyrosyadi1995@gmail.com>
2018-09-13 21:08:53 +0700

    update relasi db dan api orderproduk tanpa JWT

--- main.go
+++ main.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"net/http"
 )
 
 var db *sql.DB
 
 type registerUser struct {
-	ID int
-	Nama string
+	Id int
+	FullName string
 }
 
 type loginUser struct {
 	Id int
-	IdRegister int
 	Nama string
+	Password string
 }
 
 type orderProduk struct {
-	id int
-	idLogin int
-	nama string
-	harga int
+	Id int
+	IdLogin int
+	Nama string
+	Harga int
 }
 
 var regis []registerUser
@@ -40,15 +43,20 @@ func main() {
 	defer db.Close()
 
 	r := gin.Default()
+
 	r.GET("/regis/", RegisterUser)
 	r.GET("/regis/:nama", RegisterUserPilihan)
-	r.GET("/login/viewall", LoginUser)
-	r.GET("/login", LoginUserPilihan)
+	r.GET("/login/", LoginUser)
+	r.GET("/login/:nama", LoginUserPilihan)
+	//r.Use(logger())
+	r.GET("/order/", OrderProduk)
+	r.GET("/order/:nama", OrderProdukPilihan)
+
 	r.Run()
 }
 
 func RegisterUser(c *gin.Context) {
-	rows, err := db.Query("SELECT id, nama FROM registrasiUser")
+	rows, err := db.Query("SELECT id, fullname FROM users")
 	if err != nil {
 		log.Println(err)
 		c.JSON(500, gin.H{
@@ -62,7 +70,7 @@ func RegisterUser(c *gin.Context) {
 			var nama string
 			err = rows.Scan(&id, &nama)
 			listUser = append(listUser, registerUser{
-				ID:id, Nama:nama })
+				Id:id, FullName:nama })
 		}
 
 		c.JSON(200, gin.H{
@@ -75,7 +83,7 @@ func RegisterUser(c *gin.Context) {
 
 func RegisterUserPilihan(c *gin.Context) {
 	name := c.Param("nama")
-	rows, err := db.Query("SELECT id, nama FROM registrasiUser where nama = ?", name)
+	rows, err := db.Query("SELECT id, fullname FROM users where name = ?", name)
 	if err != nil {
 		log.Println(err)
 		c.JSON(500, gin.H{
@@ -91,7 +99,7 @@ func RegisterUserPilihan(c *gin.Context) {
 		err = rows.Scan(&id, &nama)
 		log.Println(err)
 		listUser = append(listUser, registerUser{
-			ID:id, Nama:nama })
+			Id:id, FullName:nama })
 	}
 
 	if listUser == nil {
@@ -112,7 +120,7 @@ func RegisterUserPilihan(c *gin.Context) {
 }
 
 func LoginUser(c *gin.Context) {
-	rows, err := db.Query("SELECT id, idRegister, nama FROM loginUser")
+	rows, err := db.Query("SELECT id, name, password FROM users")
 	if err != nil {
 		log.Println(err)
 		c.JSON(500, gin.H{
@@ -123,11 +131,11 @@ func LoginUser(c *gin.Context) {
 	var listUser []loginUser
 	for rows.Next(){
 		var id int
-		var idregis int
 		var nama string
-		err = rows.Scan(&id, &idregis, &nama)
+		var password string
+		err = rows.Scan(&id, &nama, &password)
 		listUser = append(listUser, loginUser{
-			Id:id, IdRegister:idregis, Nama:nama })
+			Id:id, Password:password, Nama:nama })
 	}
 
 	c.JSON(200, gin.H{
@@ -139,9 +147,8 @@ func LoginUser(c *gin.Context) {
 }
 
 func LoginUserPilihan(c *gin.Context) {
-	name := c.Query("nama")
-	idRegis := c.Query("idregis")
-	rows, err := db.Query("SELECT id, idRegister, nama FROM loginUser where nama = ? and idRegister = ?", name, idRegis)
+	name := c.Param("nama")
+	rows, err := db.Query("SELECT id, name, password FROM users where name = ?", name)
 	if err != nil {
 		log.Println(err)
 		c.JSON(500, gin.H{
@@ -153,12 +160,11 @@ func LoginUserPilihan(c *gin.Context) {
 	var listUser []loginUser
 	for rows.Next(){
 		var id int
-		var idregis int
 		var nama string
-		err = rows.Scan(&id, &idregis, &nama)
-		log.Println(err)
+		var password string
+		err = rows.Scan(&id, &nama, &password)
 		listUser = append(listUser, loginUser{
-			Id:id, IdRegister:idregis, Nama:nama })
+			Id:id, Password:password, Nama:nama })
 	}
 
 	if listUser == nil {
@@ -178,3 +184,100 @@ func LoginUserPilihan(c *gin.Context) {
 	}
 }
 
+func logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		aout := c.GetHeader("Authorization")
+		log.Println(aout)
+		if aout != "tamasia" {
+			c.JSON(400, gin.H{
+				"code": 400,
+				"success" :false,
+				"status": "Failed",
+				"message": "tidak ada akses",
+			})
+			c.Abort()
+		}
+
+	}
+}
+
+func OrderProduk(c *gin.Context) {
+	rows, err := db.Query("SELECT id, idLogin, nama, harga FROM orderProduk")
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"code": 500,
+			"message": "Internal server error",
+		})
+	}
+	var listOrder []orderProduk
+	for rows.Next(){
+		var id int
+		var idlogin int
+		var nama string
+		var harga int
+		err = rows.Scan(&id, &idlogin, &nama, &harga)
+		listOrder = append(listOrder, orderProduk{
+			Id:id, IdLogin:idlogin, Nama:nama, Harga:harga })
+	}
+
+	c.JSON(200, gin.H{
+		"code": 200,
+		"success" :true,
+		"status": "Success",
+		"OrderProduk": listOrder,
+	})
+}
+
+func OrderProdukPilihan(c *gin.Context) {
+	name := c.Param("nama")
+	rows, err := db.Query("SELECT id, idLogin, nama, harga FROM orderProduk where nama = ?", name)
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"code": 500,
+			"message": "Internal Service Error",
+		})
+	}
+
+	var listOrder []orderProduk
+	for rows.Next(){
+		var id int
+		var idlogin int
+		var nama string
+		var harga int
+		err = rows.Scan(&id, &idlogin, &nama, &harga)
+		listOrder = append(listOrder, orderProduk{
+			Id:id, IdLogin:idlogin, Nama:nama, Harga:harga })
+	}
+
+	if listOrder == nil {
+		c.JSON(404, gin.H{
+			"code": 404,
+			"success" :false,
+			"status": "Failed",
+			"message": "data tidak ditemukan",
+		})
+	}else{
+		c.JSON(200, gin.H{
+			"Code": 200,
+			"Success" :true,
+			"Status": "Success",
+			"OrderProduk": listOrder,
+		})
+	}
+}
+
+//func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
+//	var user users
+//	_ = json.NewDecoder(req.Body).Decode(&user)
+//	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+//		"username": user.Username,
+//		"password": user.Password,
+//	})
+//	tokenString, error := token.SignedString([]byte("secret"))
+//	if error != nil {
+//		fmt.Println(error)
+//	}
+//	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
+//}
